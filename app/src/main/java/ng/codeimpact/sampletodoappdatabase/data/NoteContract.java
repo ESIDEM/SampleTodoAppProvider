34e0f464f3f1696879d783afacd3a33a36d95c4d
brownjee <nsikakthompson73@gmail.com>
2017-04-06 12:01:15 +0100

    Added: Content Authority and Base_URI

--- app/src/main/java/ng/codeimpact/sampletodoappdatabase/data/NoteContract.java
+++ app/src/main/java/ng/codeimpact/sampletodoappdatabase/data/NoteContract.java
@@ -1,19 +1,42 @@
 package ng.codeimpact.sampletodoappdatabase.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Created by Nsikak  Thompson on 4/5/2017.
  */
 
-public class NoteContract implements BaseColumns {
+public class NoteContract  {
 
     public static final String DATABASE_NAME = "todo-notes.db";
+
+
+    // Name of the Content Provider, use package name by convention so that it's unique on device
+    public static final String CONTENT_AUTHORITY = "ng.codeimpact.sampletodoappdatabase";
+
+    // A path that points to the note table
+    public static final String PATH_NOTE = "note";
+
+    // Construct the Base Content Uri
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     /**
      * Define the Note table
      */
-    public static final class Note {
+    public static final class Note implements BaseColumns {
+
+
+        // Content Uri = Content Authority + Path
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_NOTE).build();
+
+        // Use MIME type prefix android.cursor.dir/ for returning multiple items
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/ng.codeimpact.sampletodoappdatabase.notes";
+        // Use MIME type prefix android.cursor.item/ for returning a single item
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/ng.codeimpact.sampletodoappdatabase.note";
+
 
         // Define table name
         public static final String TABLE_NAME = "note";
